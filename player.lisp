6979373504cae788e8bd7c26073ad7f836ff1566
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2020-11-04 23:29:04 +0100

    nicer ship

--- player.lisp
+++ player.lisp
@@ -16,7 +16,7 @@
         (make-instance 'player
                        :x (/ *window-width* 2) :y (- *window-height* 50)
                        :velocity *player-velocity*
-                       :sprite "playerShip1_blue.png"
+                       :sprite "playerShip2_red.png"
                        :bounding-radius 50)))
 
 (defun draw-missiles ()

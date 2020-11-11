d32f91d019138c741f6ec14fafd391e82e8ca629
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2020-11-11 15:54:40 +0100

    fixed indentation

--- sounds.lisp
+++ sounds.lisp
@@ -2,6 +2,6 @@
 
 (defun set-background-music (music-filename)
   (setf *background-music*
-              (sdl-mixer:load-music (asdf:system-relative-pathname
-                                     'space music-filename)))
+        (sdl-mixer:load-music (asdf:system-relative-pathname
+                               'space music-filename)))
   (sdl-mixer:play-music *background-music*))
